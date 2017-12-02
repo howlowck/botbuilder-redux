732ca7279ee1ac8ddcc362fc249f0a06c4ac8b97
Hao Luo <howlowck@gmail.com>
2017-12-02 17:20:51 -0600

    better dependencies

--- dist/BotReduxMiddleware.js
+++ dist/BotReduxMiddleware.js
@@ -1,89 +1,79 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Middleware for managing your redux between turns.
+ *
+ * __Extends BotContext:__
+ * * context.reduxStore - Redux Store
+ *
+ * __Depends on:__
+ * * context.storage - Storage provider for storing and retrieving objects
+ *
+ * **Usage Example**
+ *
+ * ```js
+ * const bot = new Bot(adapter)
+ *      .use(new MemoryStorage())
+ *      .use(new (stateFromStorage) => {
+ *        const defaultState = {requesting: null, responses: []}
+ *        return createStore(reducer, stateFromStorage || defaultState)
+ *      })
+ *      .onReceive((context) => {
+ *          context.reply(`Hello World`);
+ *      })
+ * ```
+ */
+const defaultSaveStore = (store, context) => {
+    context.reduxStore = store;
+};
+const defaultGetStore = (context) => context.reduxStore;
+class BotReduxMiddleware {
+    constructor(createStore, getStore = defaultGetStore, saveStore = defaultSaveStore) {
+        this.createStore = createStore;
+        this.getStore = getStore;
+        this.saveStore = saveStore;
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+    getInitialState(context) {
+        // Ensure storage
+        if (!context.storage) {
+            return Promise.reject(new Error(`BotReduxMiddleware: context.storage not found.`));
+        }
+        const reduxKey = this.getReduxKey(context);
+        return context.storage.read([reduxKey])
+            .then((storState) => {
+            if (Object.getOwnPropertyNames(storState).length === 0) {
+                return null;
+            }
+            delete storState[reduxKey].eTag; // removes eTag
+            return storState[reduxKey];
+        });
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    /**
-     * Middleware for managing your redux between turns.
-     *
-     * __Extends BotContext:__
-     * * context.reduxStore - Redux Store
-     *
-     * __Depends on:__
-     * * context.storage - Storage provider for storing and retrieving objects
-     *
-     * **Usage Example**
-     *
-     * ```js
-     * const bot = new Bot(adapter)
-     *      .use(new MemoryStorage())
-     *      .use(new (stateFromStorage) => {
-     *        const defaultState = {requesting: null, responses: []}
-     *        return createStore(reducer, stateFromStorage || defaultState)
-     *      })
-     *      .onReceive((context) => {
-     *          context.reply(`Hello World`);
-     *      })
-     * ```
-     */
-    const defaultSaveStore = (store, context) => {
-        context.reduxStore = store;
-    };
-    const defaultGetStore = (context) => context.reduxStore;
-    class BotReduxMiddleware {
-        constructor(createStore, getStore = defaultGetStore, saveStore = defaultSaveStore) {
-            this.createStore = createStore;
-            this.getStore = getStore;
-            this.saveStore = saveStore;
+    getReduxKey(context) {
+        const convoRef = context.conversationReference;
+        if (!convoRef.conversation) {
+            return 'conversation/default/redux';
         }
-        getInitialState(context) {
-            // Ensure storage
-            if (!context.storage) {
-                return Promise.reject(new Error(`BotReduxMiddleware: context.storage not found.`));
-            }
-            const reduxKey = this.getReduxKey(context);
-            return context.storage.read([reduxKey])
-                .then((storState) => {
-                if (Object.getOwnPropertyNames(storState).length === 0) {
-                    return null;
-                }
-                delete storState[reduxKey].eTag; // removes eTag
-                return storState[reduxKey];
-            });
-        }
-        getReduxKey(context) {
-            const convoRef = context.conversationReference;
-            if (!convoRef.conversation) {
-                return 'conversation/default/redux';
-            }
-            return 'conversation/' + convoRef.conversation.id + '/redux';
-        }
-        contextCreated(context) {
-            return this.getInitialState(context)
-                .then((stateFromStorage) => {
-                this.saveStore(this.createStore(stateFromStorage), context);
-            }).catch((error) => {
-                console.error(error);
-            });
-        }
-        postActivity(context, activities) {
-            // Ensure storage
-            if (!context.storage) {
-                return Promise.reject(new Error(`BotReduxMiddleware: context.storage not found.`));
-            }
-            const state = this.getStore(context).getState();
-            const changes = {};
-            state.eTag = '*';
-            changes[this.getReduxKey(context)] = state;
-            return context.storage.write(changes);
-        }
+        return 'conversation/' + convoRef.conversation.id + '/redux';
     }
-    exports.default = BotReduxMiddleware;
-});
+    contextCreated(context) {
+        return this.getInitialState(context)
+            .then((stateFromStorage) => {
+            this.saveStore(this.createStore(stateFromStorage), context);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+    postActivity(context, activities) {
+        // Ensure storage
+        if (!context.storage) {
+            return Promise.reject(new Error(`BotReduxMiddleware: context.storage not found.`));
+        }
+        const state = this.getStore(context).getState();
+        const changes = {};
+        state.eTag = '*';
+        changes[this.getReduxKey(context)] = state;
+        return context.storage.write(changes);
+    }
+}
+exports.default = BotReduxMiddleware;
 //# sourceMappingURL=BotReduxMiddleware.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,7 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./BotReduxMiddleware"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const BotReduxMiddleware_1 = require("./BotReduxMiddleware");
-    exports.default = BotReduxMiddleware_1.default;
-    const getStore = (context) => context.reduxStore;
-    exports.getStore = getStore;
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const BotReduxMiddleware_1 = require("./BotReduxMiddleware");
+exports.default = BotReduxMiddleware_1.default;
+const getStore = (context) => context.reduxStore;
+exports.getStore = getStore;
 //# sourceMappingURL=index.js.map
